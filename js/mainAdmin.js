d8a578d9657fa1bb94da578051bbaf298f0ae7f9
Lynn <lynn@digityze.asia>
2023-12-09 20:46:08 +0700

    update code Diệu Linh

--- js/mainAdmin.js
+++ js/mainAdmin.js
@@ -88,16 +88,16 @@ function validateFormSP() {
         errorMessages.thuongHieu = 'Thương hiệu không được để trống.';
     }
 
-    if (SLT <= 0 || '') {
-        errorMessages.SLT = 'Số lượng tồn phải lớn hơn 0.';
+    if (SLT <= 0 || SLT ==='') {
+        errorMessages.SLT = 'Số lượng tồn phải lớn hơn 0 và không được để trống.';
     }
 
     if (giaNhap <= 0 || giaNhapValue ==='') {
-        errorMessages.giaNhap = 'Giá nhập phải lớn hơn 0 không được để trống.';
+        errorMessages.giaNhap = 'Giá nhập phải lớn hơn 0 và không được để trống.';
     }
 
     if (giaBan <= giaNhap || giaBanValue === '') {
-        errorMessages.giaBan = 'Giá bán phải lớn hơn giá nhập không được để trống.';
+        errorMessages.giaBan = 'Giá bán phải lớn hơn giá nhập và không được để trống.';
     }
 
     var currentDate = new Date().toISOString().split('T')[0];
@@ -123,68 +123,3 @@ function validateFormSP() {
     return true; // Không có lỗi, có thể submit form
 }
 
-
-
-function validateFormUpdateSP() {
-    var tenSPUpdate = document.getElementById('tenSPUpdate').value;
-    var thuongHieuUpdate = document.getElementById('thuongHieuUpdate').value;
-    var SLTUpdate = document.getElementById('SLTUpdate').value;
-    var giaNhapUpdate = parseInt(document.getElementById('giaNhapUpdate').value)
-    var giaNhapUpdateValue = document.getElementById('giaNhapUpdate').value
-    var giaBanUpdateValue = document.getElementById('giaBanUpdate').value
-    var giaBanUpdate = parseInt(document.getElementById('giaBanUpdate').value)
-    var HSDUpdate = document.getElementById('HSDUpdate').value;
-
-    // Khởi tạo đối tượng chứa thông báo lỗi
-    var errorMessages = {
-        tenSPUpdate: '',
-        thuongHieuUpdate: '',
-        SLTUpdate: '',
-        giaNhapUpdate: '',
-        giaBanUpdate: '',
-        HSDUpdate: ''
-    };
-
-    // Kiểm tra điều kiện và lưu thông báo lỗi
-    if (tenSPUpdate.trim() === '') {
-        errorMessages.tenSPUpdate = 'Tên sản phẩm không được để trống.';
-    }
-
-    if (thuongHieuUpdate.trim() === '') {
-        errorMessages.thuongHieuUpdate = 'Thương hiệu không được để trống.';
-    }
-
-    if (SLTUpdate <= 0) {
-        errorMessages.SLTUpdate = 'Số lượng tồn phải lớn hơn 0.';
-    }
-
-    if (giaNhapUpdate <= 0 || giaNhapUpdateValue ==='') {
-        errorMessages.giaNhapUpdate = 'Giá nhập phải lớn hơn 0 không được để trống.';
-    }
-
-    if (giaBanUpdate.trim() <= giaNhapUpdate || giaBanUpdateValue === '') {
-        errorMessages.giaBanUpdate = 'Giá bán phải lớn hơn giá nhập và không được để trống.';
-    }
-
-    var currentDate = new Date().toISOString().split('T')[0];
-    if (HSDUpdate <= currentDate) {
-        errorMessages.HSDUpdate = 'Hạn sử dụng phải lớn hơn ngày hiện tại.';
-    }
-
-    // Hiển thị thông báo lỗi trong thẻ <small>
-    document.getElementById('tenSPUpdate-mess').innerHTML = errorMessages.tenSPUpdate;
-    document.getElementById('thuongHieuUpdate-mess').innerHTML = errorMessages.thuongHieuUpdate;
-    document.getElementById('SLTUpdate-mess').innerHTML = errorMessages.SLTUpdate;
-    document.getElementById('giaNhapUpdate-mess').innerHTML = errorMessages.giaNhapUpdate;
-    document.getElementById('giaBanUpdate-mess').innerHTML = errorMessages.giaBanUpdate;
-    document.getElementById('HSDUpdate-mess').innerHTML = errorMessages.HSDUpdate;
-
-    // Kiểm tra xem có thông báo lỗi nào không
-    for (var field in errorMessages) {
-        if (errorMessages[field] !== '') {
-            return false; // Có ít nhất một lỗi, không submit form
-        }
-    }
-
-    return true; // Không có lỗi, có thể submit form
-}
