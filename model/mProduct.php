b37b4a0ceae7f865e06c2f7dc3e85ca5ef01cfc3
Tran Huy <tranthanhhuyst2015@gmail.com>
2023-12-09 14:47:24 +0700

    Huy cập nhật xem sp

--- model/mProduct.php
+++ model/mProduct.php
@@ -19,7 +19,9 @@
             $p = new ConnectDB();
             // $con;
             if ($p->connect_DB($con)) {
-                $str = "SELECT * FROM noidungdanhgia";
+                $str = "SELECT sp.MaSanPham, nddg.NoiDungDanhGia, nddg.ThoiGianDanhGia,nddg.HinhAnh,nddg.SoSao,nddg.MaKhachHang
+                FROM sanpham sp
+                INNER JOIN noidungdanhgia nddg ON nddg.MaSanPham = sp.MaSanPham";
                 $tbl = mysqli_query($con, $str);
                 $p->closeDB($con);
                 return $tbl;
