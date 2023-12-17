953fb78324aa094a17f9760bec41de3e5054f7f9
Do Truong <dotruong0704@gmail.com>
2023-12-17 21:16:49 +0700

    fix đường dẫn

--- controller/cDetailsProduct.php
+++ controller/cDetailsProduct.php
@@ -7,9 +7,12 @@ class CDetailsProduct
     {
 
         if (isset($_REQUEST["submitAddToCart"])) {
-            
+
             if (!isset($_SESSION['MaKhachHang'])) {
-                header('location: view/dangnhap.php');
+                // echo "";
+                echo '<script>
+                window.location.href = "view/dangnhap.php";
+                </script>';
                 exit();
             }
 
@@ -22,20 +25,20 @@ class CDetailsProduct
             // kiểm tra sản phẩm đã tổn tại trong giỏ hàng chưa, nếu có trả về số lượng sản phẩm
             $resultCheckProductsAlreadyInCart = checkProductsAlreadyInCart($idProduct, $idCustommer);
 
-            if($quantity > 0) {
+            if ($quantity > 0) {
                 if ($resultCheckQuantityProduct) {
 
                     // nếu sản phẩm đã tồn tại thì cập nhật sản phẩm trong giỏ hàng
                     if ($resultCheckProductsAlreadyInCart) {
-    
+
                         // cộng số lượng thêm mới và số lượng đã có trong giỏ hàng
                         $quantity +=  $resultCheckProductsAlreadyInCart;
                         $responAddToCart = updateProductInCart($quantity, $idProduct, $idCustommer);
                     } else {
                         $responAddToCart = addToCart($quantity, $idProduct, $idCustommer);
                     }
-    
-    
+
+
                     if ($responAddToCart) {
                         echo "<script> alert('thêm sản phầm vào giỏ hàng thành công') </script>";
                     } else {
@@ -44,7 +47,7 @@ class CDetailsProduct
                 } else {
                     echo "<script>alert('số lượng sản phẩm tồn kho không đủ')</script>";
                 }
-            }else{
+            } else {
                 echo "<script>alert('số lượng sản phẩm phải lớn hơn 0')</script>";
             }
         };
@@ -91,7 +94,7 @@ function checkQuantityProduct($quantity, $idProduct)
     if ($quantity <= $quantityProductsInStock) {
         return true;
     } else {
-    
+
         return false;
     }
 }
